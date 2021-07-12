9f8087200d4c90b3e001a41c06fabd666fde3a78
Wouter Vermeij <w.vermeij@m2mobi.com>
2021-07-12 21:16:54 +0200

    add search branch feature

--- README.md
+++ README.md
@@ -21,9 +21,24 @@ exec zsh
 ## How to use
 
 After you installed the plugin and restarted your shell there are three commands you can use:
-1. `gco` (`git checkout`) - This will list all your local branches and an option to switch using a number
-2. `gcor` (`git checkout -r`) - This will list all your remote branches and an option to switch using a number
-3. `gbd` (`git branch -d`) - This will list all your local branches and the option to delete a branch. Confirmation is asked before deletion.
-4. `gm` (`git merge --no-ff`) - This will list all your local branches and an option to switch using a number
+
+**gco**
+Will list all local available branches for checkout. Choose a number from the list to checkout that branch.
+**gco \<branchname>**
+This command will immediately checkout the local branch if it exists, same like `git checkout <branchname>`.
+Example: `gco feature/add_login`
+**gco \<keyword>**
+If you have a lot of branches, the list `gco` provides can become pretty long and it can become tedious to search for the right branch. So if you know a keyword, for example it was a branch containing the text `login`, you can just write `gco login` and it will list all available branches which contain the word `login`, or if there's only 1 hit, ask you if you want to checkout that one.
+Example: 
+```
+> gco login
+> error: pathspec 'login' did not match any file(s) known to git
+> Did you mean feature/add_login? [y/n] 
+```
+
+- `gcor` (`git checkout -r`) - This will list all your remote branches and an option to switch using a number
+- `gbd` (`git branch -d`) - This will list all your local branches and the option to delete a branch. Confirmation is asked before deletion.
+- `gm` (`git merge --no-ff`) - This will list all your local branches and an option to switch using a number
 
 If you know the branchname and you are not too lazy to type, you can also append the branchname to the `gco` command. Like `gco master` and you will switch to local master branch. This also applies to the `gm` command.
+
--- git-branches.plugin.zsh
+++ git-branches.plugin.zsh
@@ -2,92 +2,143 @@
 
 # ----------------------
 # Git helper functions
+# Merge branches
 # Checkout local branches
+# Find branches by keyword
 # Checkout remote branches
 # Delete local branches
 # ----------------------
 _INPUT=
 
+branchesFile=$TMPDIR'branches'
+trap "{ rm -f $branchesFile; }" EXIT
+
+# git merge
 gm() {
+    _ACTION="merge"
+
     if [ -z "$1" ]; then
-        _listBranches "merge"
+        _retrieveBranches $_ACTION
     elif; then
-        git merge --no-ff $1
+        if ! git merge --no-ff $1; then
+            _checkIfKeywordExists $1 $_ACTION
+        fi
     fi
 }
 
+# git checkout
 gco() {
+    _ACTION="checkout"
+    
     if [ -z "$1" ]; then
-        _listBranches "checkout"
+        _retrieveBranches $_ACTION
     elif; then
-        git checkout $1
+        if ! git checkout $1; then
+            _checkIfKeywordExists $1 $_ACTION
+        fi
     fi
 }
 
+# git branch -d
 gbd() {
-    _listBranches "deletion"
+    _retrieveBranches "deletion"
 }
 
+# git checkout -t
 gcor() {
-    _listBranches "remote checkout"
+    _retrieveBranches "remote checkout"
 }
 
-_listBranches() {
+_retrieveBranches() {
+    _ACTION=$1
+    
     local NOCOLOR='\033[0m'
     local RED='\033[0;31m'
     local GREEN='\033[0;32m'
 
-    branchesFile=$TMPDIR'branches'
-    trap "{ rm -f $branchesFile; }" EXIT
-
-    case $1 in
+    case $_ACTION in
     "remote checkout") git branch -r >$branchesFile ;; # Get remote branches
     *) git branch >$branchesFile ;;                    # Get local branches
     esac
 
+    currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
+    echo "Current branch:\n${GREEN}$currentBranch${NOCOLOR}"
+    _removeCurrentBranchFromList
+    
+    echo "Branches available for $_ACTION:"
+    _listBranchesFromFile $branchesFile
+
+    _validateInput 'Branch number: '
+    branch=$(head -$_INPUT $branchesFile | tail -1 | awk '{$1=$1};1')
+
+    _handleActionForBranch $_ACTION $branch
+}
+
+_checkIfKeywordExists() {
+    _KEYWORD=$1
+    _ACTION=$2
+
+    echo "Keyword is $_KEYWORD"
+    echo "Action is $_ACTION"
+
+    grepBranches=$TMPDIR'grepBranches'
+    trap "{ rm -f $grepBranches; }" EXIT
+
+    git branch >$branchesFile
+
+    _removeCurrentBranchFromList
+
+    hitCount=$(grep -c $_KEYWORD $branchesFile)
+    grep $_KEYWORD $branchesFile >$grepBranches
+
+    case $hitCount in
+    # If 0 results, return
+    0) return ;; 
+
+    # If 1 result, give option to switch
+    1) 
+        line=($(grep -i "$_KEYWORD" $grepBranches))
+        printf "Did you mean ${GREEN}$line${NOCOLOR}? [y/n]${NOCOLOR} "
+        read confirm
+        case $confirm in
+        [Yy]*) _handleActionForBranch $_ACTION $line ;;
+            # git checkout $line
+            # return
+            # ;;
+        *) return ;;
+        esac
+        ;;
+
+    # If multiple results, let the user choose
+    *) 
+        echo "Found branches with keyword '$_KEYWORD'"
+        _listBranchesFromFile $grepBranches
+        _validateInput 'Branch number: '
+        branch=$(awk -v "line=$_INPUT" 'NR==line' $grepBranches | awk '{$1=$1};1')
+        git checkout $branch
+        ;;
+    esac
+}
+
+_removeCurrentBranchFromList() {
     n=1
     while read line; do
         if [[ $line == *"*"* ]]; then
-            currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
-            echo "Current branch:\n${GREEN}$currentBranch${NOCOLOR}"
             # Remove the current branch from the file, we cannot checkout current branch
             sed -i "" $n\d $branchesFile
         fi
         n=$((n + 1))
     done <$branchesFile
+}
 
-    echo "Branches available for $1:"
+_listBranchesFromFile() {
     n=1
     while read line; do
         if [[ $line != *"*"* ]]; then
             echo "$n. ${RED}$line${NOCOLOR}"
             n=$((n + 1))
         fi
-    done <$branchesFile
-
-    _validateInput 'Branch number: '
-    branch=$(head -$_INPUT $branchesFile | tail -1 | awk '{$1=$1};1')
-
-    case $1 in
-    "merge")
-        git merge --no-ff $branch
-    ;;
-    "remote checkout")
-        git checkout -t $branch
-        ;;
-    "checkout")
-        git checkout $branch
-        ;;
-    "deletion")
-        printf "${RED}Are you sure you want to delete branch ${NOCOLOR}$branch${RED}? [y/n]${NOCOLOR} "
-
-        read confirm
-        case "$confirm" in
-        [yY]) git branch -D $branch ;;
-        *) echo "¯\_(ツ)_/¯" ;;
-        esac
-        ;;
-    esac
+    done <$1
 }
 
 _validateInput() {
@@ -114,4 +165,26 @@ _validateInput() {
             break
         fi
     done
+}
+
+_handleActionForBranch() {   
+    _ACTION=$1
+    _BRANCH=$2
+
+    echo "_handle $_ACTION on $_BRANCH"
+
+    case $_ACTION in
+    "merge") git merge --no-ff $_BRANCH ;;
+    "remote checkout") git checkout -t $_BRANCH ;;
+    "checkout") git checkout $_BRANCH ;;
+    "deletion")
+        printf "${RED}Are you sure you want to delete branch ${NOCOLOR}$_BRANCH${RED}? [y/n]${NOCOLOR} "
+
+        read confirm
+        case "$confirm" in
+        [yY]) git branch -D $_BRANCH ;;
+        *) echo "¯\_(ツ)_/¯" ;;
+        esac
+        ;;
+    esac
 }
